3ab3c643fd4d141ac9101555b72f48e6634671b6
Timmy Shih Jun Yee <yu.shijun@masterofpi.org>
2011-03-11 10:34:52 -0800

    Initial commit

---
+++ ripple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+tokens = (
+    'SYMBOL',
+)
+
+t_SYMBOL = r'[!#$%&|*+-/:<=>?@^_~]'
+
+def t_error(t):
+    print('Illegal character: %s' % t.value[0])
+    t.lexer.skip(1)
+
+import ply.lex as lex
+lexer = lex.lex()
+
+if __name__ == '__main__':
+    from sys import argv
+    lexer.input(argv[1])
+
+    for token in lexer:
+        print('Found value')
+
